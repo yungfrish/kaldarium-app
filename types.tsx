5de8297b39543869eda4314783125c76155bae48
leonbechhaus <bechhausleon@gmail.com>
2022-11-18 11:43:57 +0100

    fix: cleanup

--- types.tsx
+++ types.tsx
@@ -1,8 +1,3 @@
-/**
- * Learn more about using TypeScript with React Navigation:
- * https://reactnavigation.org/docs/typescript/
- */
-
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
