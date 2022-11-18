24c8047acb039e9c9c73b625cb77147361195876
Leon Bechhaus <lbechhaus@noriskshop.de>
2022-11-18 11:35:58 +0100

    add basic expo/react-native build

---
+++ screens/TabTwoScreen.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, Text, View } from "react-native";
+
+import EditScreenInfo from "../components/EditScreenInfo";
+
+export default function TabTwoScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Tab Two</Text>
+      <View style={styles.separator} />
+      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
+    backgroundColor: "#eee",
+  },
+});
