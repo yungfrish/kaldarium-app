fec8a62a7a9ad4beb6b39d99de7b133382376264
Benedikt Matern <benediktmatern@gmail.com>
2023-01-05 15:43:42 +0100

    removed boxShadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -70,11 +70,6 @@ module.exports = {
         DEFAULT: "8px",
         xl: "40px",
       },
-      boxShadow: {
-        xl: "0 24px 32px 0 rgba(0, 0, 0, 0.2)",
-        md: "0 12px 24px 0 rgba(0, 0, 0, 0.2)",
-        sm: "0 8px 24px 0 rgba(0, 0, 0, 0.2)",
-      },
     },
   },
   plugins: [],
